69daffcc1fc810494c50f0aaba6fe474515d856b
Sagi Carmel <ace.code.pt@gmail.com>
2024-01-20 21:52:44 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "astro/config";
+import sidecar from "astro-sidecar";
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    port: 5000,
+  },
+
+  integrations: [
+    //
+    sidecar({
+      entryPoints: [
+        //
+        "./src/server/websocket.ts",
+      ],
+    }),
+  ],
+});
---
+++ consts.ts
@@ -0,0 +1,2 @@
+export const WEB_SOCKET_DEV_PORT = 5002;
+export const WEB_SOCKET_DEV_URL = `ws://localhost:${WEB_SOCKET_DEV_PORT}`;
